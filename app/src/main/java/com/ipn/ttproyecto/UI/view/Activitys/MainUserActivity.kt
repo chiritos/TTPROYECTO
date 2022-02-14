804e72578636f47bcd368037524b282f0130144a
chiritos <glijito@hotmail.com>
2022-02-14 01:39:15 -0600

    1.- cambios en primera fase,de structura de proyecto

--- app/src/main/java/com/ipn/ttproyecto/UI/view/Activitys/MainUserActivity.kt
+++ app/src/main/java/com/ipn/ttproyecto/UI/view/Activitys/MainUserActivity.kt
@@ -27,10 +27,6 @@ class MainUserActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMainUser.toolbar)
 
-        binding.appBarMainUser.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main_user)
--- app/src/main/java/com/ipn/ttproyecto/UI/view/Activitys/SelectPersonActivity.kt
+++ app/src/main/java/com/ipn/ttproyecto/UI/view/Activitys/SelectPersonActivity.kt
@@ -3,10 +3,27 @@ package com.ipn.ttproyecto.UI.view.Activitys
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ipn.ttproyecto.R
+import com.ipn.ttproyecto.databinding.ActivityPersonSelectBinding
+import com.ipn.ttproyecto.databinding.ActivityUsuarioMainBinding
 
 class SelectPersonActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPersonSelectBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_person_select)
+
+        binding = ActivityPersonSelectBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnUser.setOnClickListener {
+
+        }
+
+        binding.btnUser.setOnClickListener {
+
+        }
+
     }
 }
